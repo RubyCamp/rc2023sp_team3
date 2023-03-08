5b6b56f5f14f344776c3142cfe2ac9fe5d516b47
李展雲 <239398873@qq.com>
2023-03-08 10:20:56 +0900

    stage2

--- main.rb
+++ main.rb
@@ -3,21 +3,25 @@ require 'dxruby'
 require_relative 'teki'
 require_relative 'player'
 require_relative 'judge'
+require_relative 'stage2'
 
 Window.width = 800
 Window.height = 600
-Window.caption = "RubyCamp 2023SP Sample Game"
+Window.bgcolor = C_BLACK
+Window.caption = "島根への旅"
 
 # ゲーム開始時点で実行する最初のシーンディレクターを生成
 player = Player.new
 teki = Teki.new
 judge = Judge.new(player,teki)
+stage2 = Stage2.new
 
 imgs = [@teki_image, @player_img]
 
 Window.loop do
     teki.play
     player.play
+    stage2.play
     # judge.hantei
     Sprite.draw(imgs)
     judge.hantei
--- stage2.rb
+++ stage2.rb
@@ -1,9 +1,9 @@
-require 'dxruby'
- Window.bgcolor = C_CYAN
- Window.caption = "島根への旅"
- font = Font.new(32)
- font = Font.new(32)
-Window.loop do
- Window.draw_font(0, 400, "東京", font, {:color => C_BLACK})
- Window.draw_font(500, 400, "出雲大社", font, {:color => C_BLACK})
+class Stage2 
+ def initialize
+  @font = Font.new(32)
+ end
+ def play
+  Window.draw_font(0, 400, "東京", @font, {:color => C_WHITE})
+  Window.draw_font(500, 400, "出雲大社", @font, {:color => C_WHITE})
+ end
 end
